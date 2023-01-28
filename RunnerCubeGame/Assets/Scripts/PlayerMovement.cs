9f7fb4eb21f77c00b74529bd8878e147fd225d84
Bilal-Yucell <bilallyucell@gmail.com>
2023-01-28 22:53:16 +0300

    Commit 13

---
+++ RunnerCubeGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+
+    public float sidewaysForce = 500f;
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -3f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+    }
+}
---
+++ RunnerCubeGame/Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+// using UnityEngine.TextCore;
+
+
+public class Score : MonoBehaviour
+{
+
+    public Transform player;
+    public TextMeshProUGUI scoreText;
+
+    void Update()
+    {
+        scoreText.SetText(player.position.z.ToString("0"));
+    }
+}
